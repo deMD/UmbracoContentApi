3453c820fd6626e9da825df0b7b4f4ce02e088aa
Dave de Moel <davedemoel@hotmail.com>
2019-06-28 23:25:23 +0200

    Initial commit

---
+++ src/UmbracoContentApi/UmbracoContentApi.Web/Controllers/EntryApiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Umbraco.Web;
+using Umbraco.Web.Composing;
+using Umbraco.Web.WebApi;
+
+namespace UmbracoContentApi.Web.Controllers
+{
+    public class EntryApiController : UmbracoApiController
+    {
+        private readonly UmbracoHelper _umbracoHelper;
+
+        public EntryApiController()
+        {
+            _umbracoHelper = Current.UmbracoHelper;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var content = _umbracoHelper.Content(id);
+            
+            return Json(content, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+    }
+}
