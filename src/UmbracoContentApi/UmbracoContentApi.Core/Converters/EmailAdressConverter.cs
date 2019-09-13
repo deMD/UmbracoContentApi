1aff352cfc9758662a07ee2beda3f6e7d1cb8093
Dave de Moel <d.demoel@wearetriple.com>
2019-09-13 14:20:47 +0200

    Added emailaddress converter

---
+++ src/UmbracoContentApi/UmbracoContentApi.Core/Converters/EmailAdressConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbracoContentApi.Core.Converters
+{
+    public class EmailAdressConverter : IConverter
+    {
+        public string EditorAlias => "Umbraco.EmailAddress";
+
+        public object Convert(object value)
+        {
+            return value;
+        }
+    }
+}
