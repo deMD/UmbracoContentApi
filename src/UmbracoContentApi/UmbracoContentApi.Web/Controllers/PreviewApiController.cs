708fb01c2de97e9016a9148bae2038b5d0e92bae
Dave de Moel <d.demoel@wearetriple.com>
2019-09-25 10:52:25 +0200

    Updated preview with method that returns preview content

--- src/UmbracoContentApi/UmbracoContentApi.Web/Controllers/PreviewApiController.cs
+++ src/UmbracoContentApi/UmbracoContentApi.Web/Controllers/PreviewApiController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Core.Services;
-using Umbraco.Web.PublishedCache;
 using Umbraco.Web.WebApi;
 using UmbracoContentApi.Core.Models;
 using UmbracoContentApi.Core.Resolvers;
@@ -14,27 +11,19 @@ namespace UmbracoContentApi.Web.Controllers
     public class PreviewApiController : UmbracoApiController
     {
         private readonly Lazy<IContentResolver> _contentResolver;
-        private readonly IPublishedSnapshotService _publishedSnapshotService;
-        private readonly IUserService _userService;
-        private readonly IContentService _contentService;
 
-        public PreviewApiController(Lazy<IContentResolver> contentResolver, IPublishedSnapshotService publishedSnapshotService, IUserService userService, IContentService contentService)
+        public PreviewApiController(Lazy<IContentResolver> contentResolver)
         {
             _contentResolver = contentResolver;
-            _publishedSnapshotService = publishedSnapshotService;
-            _userService = userService;
-            _contentService = contentService;
         }
 
         [Route("{id:guid}")]
         [ResponseType(typeof(ContentModel))]
         public IHttpActionResult Get(Guid id)
         {
-            var val = _publishedSnapshotService.EnterPreview(_userService.GetUserById(-1), _contentService.GetById(id).Id);
-
-            IPublishedContent content = Umbraco.Content(id);
-
-            ContentModel contentModel = _contentResolver.Value.ResolveContent(content);
+            var preview = UmbracoContext.Content.GetById(true, id);
+            
+            ContentModel contentModel = _contentResolver.Value.ResolveContent(preview);
 
             return Ok(contentModel);
         }
