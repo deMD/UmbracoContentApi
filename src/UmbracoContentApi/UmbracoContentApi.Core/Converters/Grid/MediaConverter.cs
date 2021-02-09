1015b4971bdf3afc1a860a3388d42df2c45787f8
Dave de Moel <d.demoel@wearetriple.com>
2021-02-09 09:53:40 +0100

    Added grid editor converters

---
+++ src/UmbracoContentApi/UmbracoContentApi.Core/Converters/Grid/MediaConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using Newtonsoft.Json;
+using UmbracoContentApi.Core.Models.Grid;
+
+namespace UmbracoContentApi.Core.Converters.Grid
+{
+    public class MediaConverter : IGridConverter
+    {
+        private readonly Uri _cdnUrl;
+        
+        public MediaConverter()
+        {
+            Uri.TryCreate(ConfigurationManager.AppSettings["ContentApi:CdnUrl"], UriKind.Absolute, out _cdnUrl);
+        }
+
+        public string EditorAlias => "media";
+
+        public object Convert(object value)
+        {
+            var image = JsonConvert.DeserializeObject<Media>(value.ToString());
+            return image != null
+                ? new
+                {
+                    image.Id,
+                    ImageUrl = _cdnUrl == null
+                        ? new Uri(image.Image, UriKind.Relative)
+                        : new Uri(_cdnUrl, image.Image)
+                }
+                : null;
+        }
+    }
+}
